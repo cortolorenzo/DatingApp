81d797fe61d0eeae3622cf2d8fc4e555736756a1
Michał Miszkurka <miszkurkam92@gmail.com>
2021-07-25 21:59:27 +0200

    Initial

---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,39 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  //@Input() usersFromHomeComponent: any;
+  @Output() cancelRegister = new EventEmitter();
+
+
+  model: any = {};
+
+  constructor(private accountSerice: AccountService, private toastr: ToastrService) { }
+
+  ngOnInit(): void {
+  }
+
+  register(){
+    this.accountSerice.register(this.model).subscribe(res => {
+      console.log(res);
+      this.cancel();
+    }, error => {
+      console.log(error);
+      this.toastr.error(error.error);
+    })
+    
+  }
+
+  cancel(){
+    this.cancelRegister.emit(false);
+  }
+
+  
+
+}
