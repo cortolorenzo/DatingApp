4cce23e15e8f34976a8a0020476217a734e992ab
Michał Miszkurka <miszkurkam92@gmail.com>
2021-08-27 00:12:05 +0200

    reactiveforms

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
@@ -13,20 +14,48 @@ export class RegisterComponent implements OnInit {
 
 
   model: any = {};
+  registerForm: FormGroup;
 
   constructor(private accountSerice: AccountService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
+    this.initializeForm();
   }
 
-  register(){
-    this.accountSerice.register(this.model).subscribe(res => {
-      console.log(res);
-      this.cancel();
-    }, error => {
-      console.log(error);
-      this.toastr.error(error.error);
+  initializeForm(){
+    this.registerForm = new FormGroup({
+      username: new FormControl('',Validators.required),
+      password: new FormControl('',[Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
+      confirmPassword: new FormControl('', [Validators.required, this.matchValues('password')])
     })
+
+    this.registerForm.controls.password.valueChanges.subscribe(() => {
+        this.registerForm.controls.confirmPassword.updateValueAndValidity();
+    })
+
+  }
+
+  matchValues(matchTo:string): ValidatorFn {
+    return (control: AbstractControl): {[key: string]: any} | null => {
+      const forbidden = control?.parent?.controls as any;
+      return (forbidden) 
+        ? (control?.value === forbidden[matchTo]?.value) ? null : {isMatching: true}
+        : null;
+    }
+  }
+  
+
+
+
+  register(){
+    console.log(this.registerForm.value);
+    // this.accountSerice.register(this.model).subscribe(res => {
+    //   console.log(res);
+    //   this.cancel();
+    // }, error => {
+    //   console.log(error);
+    //   this.toastr.error(error.error);
+    // })
     
   }
 
