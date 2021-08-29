a2ac97b3261b71162dd3103b8ef3e6b43b719a8a
Michał Miszkurka <miszkurkam92@gmail.com>
2021-08-29 20:25:36 +0200

    register form extension

--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -5,7 +5,7 @@ import { ToastrModule } from 'ngx-toastr';
 import {TabsModule} from 'ngx-bootstrap/tabs';
 import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 import { FileUploadModule } from 'ng2-file-upload';
-
+import {BsDatepickerModule} from 'ngx-bootstrap/datepicker';
 
 
 @NgModule({
@@ -18,7 +18,8 @@ import { FileUploadModule } from 'ng2-file-upload';
     }),
     TabsModule.forRoot(),
     NgxGalleryModule,
-    FileUploadModule
+    FileUploadModule,
+    BsDatepickerModule.forRoot()
 
 
 
@@ -28,7 +29,8 @@ exports: [
   ToastrModule,
   TabsModule,
   NgxGalleryModule,
-  FileUploadModule
+  FileUploadModule,
+  BsDatepickerModule
 ]
 
 })
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
@@ -11,22 +12,31 @@ import { AccountService } from '../_services/account.service';
 export class RegisterComponent implements OnInit {
   //@Input() usersFromHomeComponent: any;
   @Output() cancelRegister = new EventEmitter();
-
-
-  model: any = {};
+  maxDate: Date;
   registerForm: FormGroup;
 
-  constructor(private accountSerice: AccountService, private toastr: ToastrService) { }
+  validationErrors: string[] = [];
+
+  constructor(private accountSerice: AccountService, private toastr: ToastrService, private fb: FormBuilder, private router: Router) { }
 
   ngOnInit(): void {
     this.initializeForm();
+    this.maxDate = new Date();
+    this.maxDate.setFullYear(this.maxDate.getFullYear() -18);
   }
 
   initializeForm(){
-    this.registerForm = new FormGroup({
-      username: new FormControl('',Validators.required),
-      password: new FormControl('',[Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
-      confirmPassword: new FormControl('', [Validators.required, this.matchValues('password')])
+    this.registerForm = this.fb.group({
+      gender: ['male'],
+      username: ['',Validators.required],
+      knownAs: ['',Validators.required],
+      dateOfBirth: ['',Validators.required],
+      city: ['',Validators.required],
+      country: ['',Validators.required],
+
+
+      password: ['',[Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
+      confirmPassword: ['', [Validators.required, this.matchValues('password')]]
     })
 
     this.registerForm.controls.password.valueChanges.subscribe(() => {
@@ -48,14 +58,14 @@ export class RegisterComponent implements OnInit {
 
 
   register(){
-    console.log(this.registerForm.value);
-    // this.accountSerice.register(this.model).subscribe(res => {
-    //   console.log(res);
-    //   this.cancel();
-    // }, error => {
-    //   console.log(error);
-    //   this.toastr.error(error.error);
-    // })
+    //console.log(this.registerForm.value);
+    this.accountSerice.register(this.registerForm.value).subscribe(res => {
+      this.router.navigateByUrl('/members')
+    }, error => {
+      this.validationErrors = error;
+
+      
+    })
     
   }
 
