63f34330e7c28d5dce4a04b6d43ca381cc9260ef
Michał Miszkurka <miszkurkam92@gmail.com>
2021-09-28 09:47:33 +0200

    sdf

--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -8,23 +8,18 @@ import { MessageService } from 'src/app/_services/message.service';
   styleUrls: ['./member-messages.component.css']
 })
 export class MemberMessagesComponent implements OnInit {
-
+  @Input() messages: Message[];
   @Input() username: string;
-  messages: Message[];
 
 
 
-  constructor(private messageService: MessageService) { }
+  constructor() { }
 
   ngOnInit(): void {
 
-    this.loadMessages();
+    
   }
 
-  loadMessages(){
-    this.messageService.getMessageThread(this.username).subscribe(messages => {
-      this.messages = messages;
-    })
-  }
+ 
 
 }
