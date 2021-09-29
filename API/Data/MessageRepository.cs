7175807c1570dd7f302ecf3206b8dd5aeb0febfb
Michael Miszkurka <miszkurkam92@gmail.com>
2021-09-29 18:20:02 +0200

    asdf

--- API/Data/MessageRepository.cs
+++ API/Data/MessageRepository.cs
@@ -33,7 +33,10 @@ namespace API.Data
 
         public async Task<Message> Getmessage(int id)
         {
-            return await _context.Messages.FindAsync(id);
+            return await _context.Messages
+            .Include(u => u.Sender)
+            .Include(u => u.Recipient)
+            .SingleOrDefaultAsync(x => x.Id == id);
         }
 
         public async Task<PagedList<MessageDto>> GetMessagesForUser(MessageParams messageParams)
@@ -44,9 +47,9 @@ namespace API.Data
 
             query = messageParams.Container switch
             {
-                "Inbox" => query.Where(u => u.Recipient.UserName == messageParams.Username),
-                "Outbox" => query.Where(u => u.Sender.UserName == messageParams.Username),
-                _ => query.Where(u => u.Recipient.UserName == messageParams.Username && u.DateRead == null)
+                "Inbox" => query.Where(u => u.Recipient.UserName == messageParams.Username &&  u.RecipientDeleted == false),
+                "Outbox" => query.Where(u => u.Sender.UserName == messageParams.Username && u.Senderdeleted == false),
+                _ => query.Where(u => u.Recipient.UserName == messageParams.Username && u.DateRead == null && u.RecipientDeleted == false) // unreadmessages
             };
 
             var messages = query.ProjectTo<MessageDto>(_mapper.ConfigurationProvider);
@@ -58,9 +61,9 @@ namespace API.Data
             var messages = await _context.Messages
                     .Include(u => u.Sender).ThenInclude(p => p.Photos)
                     .Include(u => u.Recipient).ThenInclude(p => p.Photos)
-                    .Where( m=> m.Recipient.UserName == currentUsername
+                    .Where( m=> m.Recipient.UserName == currentUsername && m.RecipientDeleted == false
                         && m.Sender.UserName == recipientUsername || m.Recipient.UserName == recipientUsername
-                        && m.Sender.UserName == currentUsername
+                        && m.Sender.UserName == currentUsername && m.Senderdeleted == false
                     
                     
                     )
